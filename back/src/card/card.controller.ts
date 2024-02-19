fdb23aa75011eb5a50c79d96e2d445552c189778
teddy <teddy.gauthier@outlook.com>
2024-02-20 00:50:16 +0100

    feat(card) : add route answer and quizz

--- back/src/card/card.controller.ts
+++ back/src/card/card.controller.ts
@@ -1,7 +1,6 @@
 import {
   ApiTags,
   ApiParam,
-  ApiBody,
   ApiQuery,
   ApiOperation,
   ApiResponse,
@@ -15,26 +14,42 @@ import {
   Query,
   Delete,
   Patch,
+  HttpCode,
 } from '@nestjs/common';
 import { CardService } from './card.service';
 import { Card } from './entity/card.entity';
 import { CreateCardDto } from './dto/create-card.dto';
 import { UpdateCardDto } from './dto/update-card.dto';
 import { BaseCardDto } from './dto/base-card.dto';
+import { AnswerCardDto } from './dto/answer-card.dto';
 
 @ApiTags('Cards')
 @Controller('cards')
 export class CardController {
   constructor(private readonly cardService: CardService) {}
 
+  @Get('quizz')
+  @ApiOperation({ summary: 'Get a quiz' })
+  @ApiQuery({ name: 'date', description: 'Date of the quiz' })
+  @ApiResponse({
+    status: 200,
+    description: 'Return a quiz',
+    type: [BaseCardDto],
+  })
+  quiz(@Query('date') date: string): Promise<Card[]> {
+    return this.cardService.quizz(date);
+  }
+
   @Get()
   @ApiOperation({ summary: 'Get all cards' })
+  @ApiQuery({ name: 'tag', description: 'tag card' })
   @ApiResponse({
     status: 200,
     description: 'Return all cards.',
     type: [BaseCardDto],
   })
-  findAll(): Promise<Card[]> {
+  findAll(@Query('tag') tag: string): Promise<Card[]> {
+    if (tag) return this.cardService.findByTag(tag);
     return this.cardService.findAll();
   }
 
@@ -85,26 +100,16 @@ export class CardController {
 
   @Patch(':id/answer')
   @ApiParam({ name: 'id', description: 'Card id' })
-  @ApiBody({
-    schema: { type: 'object', properties: { isValid: { type: 'boolean' } } },
-  })
   @ApiOperation({ summary: 'Update a card answer by id' })
+  @ApiResponse({
+    status: 204,
+    description: 'The card has been successfully updated',
+  })
+  @HttpCode(204)
   answer(
     @Param('id') id: string,
-    @Body('isValid') isValid: boolean,
-  ): Promise<Card> {
+    @Body() { isValid }: AnswerCardDto,
+  ): Promise<void> {
     return this.cardService.answer(id, isValid);
-  }
-
-  @Get('quizz')
-  @ApiOperation({ summary: 'Get a quiz' })
-  @ApiQuery({ name: 'date', description: 'Date of the quiz' })
-  @ApiResponse({
-    status: 200,
-    description: 'Return a quiz',
-    type: [BaseCardDto],
-  })
-  quiz(@Query('date') date: Date): Promise<Card[]> {
-    return this.cardService.quizz();
   }
 }
--- back/src/card/card.service.ts
+++ back/src/card/card.service.ts
@@ -1,9 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Card } from './entity/card.entity';
 import { CreateCardDto } from './dto/create-card.dto';
 import { UpdateCardDto } from './dto/update-card.dto';
+import { nextCardCategory, nextReviewDate } from './utils/card.utils';
+import { CardCategory } from './interface/card.interface';
+import { LessThanOrEqual } from 'typeorm';
 
 @Injectable()
 export class CardService {
@@ -25,7 +28,7 @@ export class CardService {
   }
 
   create(createCardDto: CreateCardDto): Promise<Card> {
-    const card = new Card();
+    const card: Card = new Card();
     card.question = createCardDto.question;
     card.answer = createCardDto.answer;
     card.tag = createCardDto.tag;
@@ -41,13 +44,26 @@ export class CardService {
     await this.cardRepository.delete(id);
   }
 
-  answer(id: string, isValid: boolean): Promise<Card> {
-    // TODO: Implement answer logic
-    return this.cardRepository.save({ id, isValid });
+  async answer(id: string, isValid: boolean): Promise<void> {
+    const card: Card = await this.cardRepository.findOneBy({ id });
+    if (!card) throw new HttpException('Card not found', HttpStatus.NOT_FOUND);
+
+    if (isValid && card.category === CardCategory.SEVENTH) {
+      await this.cardRepository.delete(id);
+      return;
+    }
+
+    const category: CardCategory = nextCardCategory(card.category);
+    card.category = isValid ? category : CardCategory.FIRST;
+    card.nextReview = nextReviewDate(isValid ? category : CardCategory.FIRST);
+
+    await this.cardRepository.save(card);
   }
 
-  quizz(): Promise<Card[]> {
-    // TODO: Implement quizz logic
-    return this.cardRepository.find();
+  quizz(date: string): Promise<Card[]> {
+    const reviewDate: Date = new Date(date);
+    return this.cardRepository.findBy({
+      nextReview: LessThanOrEqual(reviewDate),
+    });
   }
 }
